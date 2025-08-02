63177a769a730c1980ccc0314406801ce9e6e4a6
Jordan Abraham <personal.jordanabraham@gmail.com>
2025-08-02 17:21:29 -0400

    chore: jag test prints

--- io/jag_test.go
+++ io/jag_test.go
@@ -5,20 +5,20 @@ import "testing"
 func TestGnomeballButtonsHash(t *testing.T) {
 	result := Hash("gnomeball_buttons.dat")
 	if result != 22834782 {
-		t.Errorf("TestGnomeballButtons expected 22834782, got %d", result)
+		t.Errorf("TestGnomeballButtonsHash expected 22834782, got %d", result)
 	}
 }
 
 func TestHeadiconsHash(t *testing.T) {
 	result := Hash("headicons.dat")
 	if result != -288954319 {
-		t.Errorf("TestHeadicons expected -288954319, got %d", result)
+		t.Errorf("TestHeadiconsHash expected -288954319, got %d", result)
 	}
 }
 
 func TestHitmarksHash(t *testing.T) {
 	result := Hash("hitmarks.dat")
 	if result != -1502153170 {
-		t.Errorf("TestHitmarks expected -1502153170, got %d", result)
+		t.Errorf("TestHitmarksHash expected -1502153170, got %d", result)
 	}
 }
