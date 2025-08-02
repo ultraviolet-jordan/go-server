fc9c5a12138f3eae9ea697a37fb1ca863fab50dd
Jordan Abraham <personal.jordanabraham@gmail.com>
2025-08-02 18:36:58 -0400

    chore: trig thing

--- math/trig.go
+++ math/trig.go
@@ -7,7 +7,7 @@ const (
 	DegreesRange           = 16384
 	DegreesMask            = DegreesRange - 1
 	RadiansToDegreesFactor = float64(DegreesRange) / Tau
-	Size                   = math.Pi / (float64(DegreesRange) / 2)
+	TrigRange              = math.Pi / (float64(DegreesRange) / 2)
 )
 
 // ----
@@ -20,7 +20,7 @@ type Trig struct {
 func NewTrig() *Trig {
 	trig := &Trig{}
 	for i := range DegreesRange {
-		s, c := math.Sincos(float64(i) * Size)
+		s, c := math.Sincos(float64(i) * TrigRange)
 		trig.sin[i] = int32(s * DegreesRange)
 		trig.cos[i] = int32(c * DegreesRange)
 	}
@@ -31,16 +31,16 @@ func Radians(x int32) float64 {
 	return (float64(x&DegreesMask) / DegreesRange) * Tau
 }
 
-func Atan2(y, x int32) int32 {
+func Atan2(y int32, x int32) int32 {
 	return int32(math.Round(math.Atan2(float64(y), float64(x))*RadiansToDegreesFactor)) & DegreesMask
 }
 
 // ----
 
 func (t *Trig) Sin(value int32) int32 {
-	return t.sin[int(value&DegreesMask)]
+	return t.sin[value&DegreesMask]
 }
 
 func (t *Trig) Cos(value int32) int32 {
-	return t.cos[int(value&DegreesMask)]
+	return t.cos[value&DegreesMask]
 }
